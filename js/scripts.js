e4ed7cf769523d736635fd390a16c5a8669657c7
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-04-05 12:51:54 -0400

    Added JS and HTML to pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,12 @@
 // let doubleSize = size * 2;
 // document.write(doubleSize);
 
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+// let size = 100;
+// let doubleSize = size * 2;
+// let minSize = (doubleSize * 2) - (size / 2);
+// document.write(minSize);
+
+let favoriteFood = 'Tacos';
+document.write(favoriteFood);
+
+alert('Hello World!');
