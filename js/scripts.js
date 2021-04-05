ef43d23e2a3ca345c271cf16e82b2c9f48cac908
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-04-05 14:28:01 -0400

    JS File Change

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-// let myName = 'Jonathon';
-// document.write(myName);
-
-// let simpleAddition = 2 + 2;
-// document.write(simpleAddition);
-
-// let size = 100;
-// let doubleSize = size * 2;
-// document.write(doubleSize);
-
-// let size = 100;
-// let doubleSize = size * 2;
-// let minSize = (doubleSize * 2) - (size / 2);
-// document.write(minSize);
-
 let favoriteFood = 'Tacos';
 document.write(favoriteFood);
 
