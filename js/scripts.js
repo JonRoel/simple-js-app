00d564f9515e90ad705c1f64ba177300e3999eb4
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-04-29 15:25:13 -0400

    Minified JS and CSS
    Added minified js and css to dist folder relinked necessary files in index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,18 @@
 var pokemonRepo = (function () {
  var pokemonList = [];
-  
  // Loads API url
-  
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=248';
-  
   // checks for object and allows addiing to array function
   function add(pokemon) {
     if (typeof pokemon === 'object') {
       pokemonList.push(pokemon);
-    } 
-  };
-
+    }
+  }
   //Gets the entire list from pokemonList Array
-  
   function getAll() {
     return pokemonList;
   };
-
   // Add each item to li
-
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
     let listPokemon = document.createElement('li');
@@ -37,8 +30,6 @@ var pokemonRepo = (function () {
       showDetails(pokemon)
     });
   }
-
-
     function loadList() {
       return fetch(apiUrl).then(function (response) {
         return response.json();
@@ -50,15 +41,12 @@ var pokemonRepo = (function () {
             imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`,
           };
           add(pokemon);
-          //console.log.indexOf(pokemon);
         });
       }).catch(function (e) {
         console.error(e);
       })
     }
-
     // Load Details of each
-
     function loadDetails(item) {
       let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -75,77 +63,60 @@ var pokemonRepo = (function () {
         console.error(e);
         });
       }
-
     // Show Details
-
     function showDetails(pokemon) {
       pokemonRepo.loadDetails(pokemon).then(function () {
       showModal(pokemon);
       console.log(pokemon);
       });
     }
-
     function showModal(pokemon) {
       // Define each model element
       let modalHeader = $('.modal-header');
       let modalTitle = $('.modal-title');
       let modalBody = $('.modal-body');
       let modalFooter = $('.modal-footer');
-
       // Empty modal content
       modalTitle.empty();
       modalBody.empty();
       modalHeader.empty();
-
       //Get the pokemon name
       let nameElement = $('<h2' + pokemon.name + '</h2>');
-        
       let modal = document.createElement('div');
       modal.classList.add('modal');
-
-      
-
       //get the Image
       let imageElementFront = $('<img class="pokemon-image">');
       imageElementFront.attr('src', pokemon.imageUrlFront);
-
+      // get image back
       let imageElementBack = $('<img class="pokemon-image">');
       imageElementBack.attr('src', pokemon.imageUrlBack);
-
       //get the height of the pokemon
       let heightElement = $('<p> Height: ' + pokemon.height + '</p>');
-    
-        //get the weight of the pokemon
-
+      //get the weight of the pokemon
       let weightElement = $('<p> Weight: ' + pokemon.weight + '</p>');
-
       // get the Type of each pokemon, these are objects with the arrays
       let typesDiv = document.createElement('div');
         typesDiv.classList.add('type-wrapper');
         typesDiv.classList.add('row');
-      
         pokemon.types.forEach((type) => {
         let typesElement = document.createElement('div');
         let typesText = document.createElement('p');
         typesText.innerText = type.type.name;
-  
         typesElement.classList.add('type');
         typesElement.classList.add('col');
         typesElement.classList.add(type.type.name);
         typesElement.appendChild(typesText);
         typesDiv.appendChild(typesElement);
       });
-
+      // Append Modal Elements
       modalTitle.append(nameElement);
       modalBody.append(imageElementFront);
       modalBody.append(imageElementBack);
-      modalBody.append(typesDiv); 
+      modalBody.append(typesDiv);
       modalBody.append(heightElement);
       modalBody.append(weightElement);
-
   }
 // END OF MODAL
-
   return {
     showDetails: showDetails,
     addListItem: addListItem,
@@ -154,18 +125,29 @@ var pokemonRepo = (function () {
     loadList: loadList,
     loadDetails: loadDetails,
   };
-
-
 })();
-
 //console.log(pokemonRepo.getAll());
-
 pokemonRepo.loadList().then(function() {
   pokemonRepo.getAll().forEach(function(pokemon) {
     pokemonRepo.addListItem(pokemon);
   })
 });
-
-
-
-
+// Search function
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("pokemon-list");
+  li = ul.getElementsByTagName('li');
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("button")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+  };
